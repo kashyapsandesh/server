96909cb061b4f6355efd8c846471e764cf953ae8
Sandesh Ghimire <88843968+kashyapsandesh@users.noreply.github.com>
2024-12-10 08:27:17 +0545

    use : DATABASE_URL=postgresql://<postgres_user>:<postgres_password>@db:5432/mydb

--- src/db/index.ts
+++ src/db/index.ts
@@ -1,6 +1,6 @@
 import { drizzle } from "drizzle-orm/node-postgres";
 import { Pool } from "pg";
 const pool = new Pool({
-  connectionString: "postgresql://postgres:root@localhost:5432/mydb",
+  connectionString: "postgresql://postgres:root@db:5432/mydb",
 });
 export const db = drizzle(pool);
